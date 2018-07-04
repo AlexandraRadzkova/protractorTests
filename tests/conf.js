3e144ad60b8439694d4ccda67bd42d8b319d9130
AlexandraRadzkova <alexandra.radzkova@gmail.com>
2018-07-04 12:20:12 +0300

    refactor

--- tests/conf.js
+++ tests/conf.js
@@ -1,4 +1,11 @@
 exports.config = {
-	seleniumAddress: 'http://localhost:4444/wd/hub',
-	specs: ['test-spec.js'],
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    capabilities: { browserName: 'chrome' },
+    specs: ['test-spec.js'],
+    onPrepare: function() {
+        browser.driver
+            .manage()
+            .window()
+            .setSize(1920, 1080)
+    },
 }
--- tests/integration-conf.js
+++ tests/integration-conf.js
@@ -1,4 +1,11 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
+    capabilities: { browserName: 'chrome' },
     specs: ['integration-spec.js'],
+    onPrepare: function() {
+        browser.driver
+            .manage()
+            .window()
+            .setSize(1920, 1080)
+    },
 }
